a5fe5c13fcf73817d37bc4713f8195dbce34b08f
= <=>
2025-12-05 00:40:14 -0300

    chore: configure github pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
   return (
     <CartProvider>
       <AuthProvider>
-        <Router>
+        <Router basename="/PaginaWeb-Marce">
           <div className="min-h-screen bg-gray-50 flex flex-col">
             <Header />
             <div className="flex-grow">
